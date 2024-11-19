28c2ac56fca80c5fcbb4b3c34aca39bf6f424ba9
DanielSaldanha <danielpedrosaldanha@gmail.com>
2024-11-19 11:28:09 -0300

    leva 8

--- LuaPula/Assets/Script/Elevador.cs
+++ LuaPula/Assets/Script/Elevador.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Elevador : MonoBehaviour
 {
+    public GameObject engrenagem, limitador, limitador2, posicional;
+    public float z = 0;
+    public float velocidadeRotacionatoria;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] float Speed = 100, Maxtime, time;
     [SerializeField] bool Aviso;
@@ -20,22 +23,33 @@ public class Elevador : MonoBehaviour
         {
 
             Subir();
+            Destroy(limitador);
         }
         if(Aviso == true)
         {
             time += Time.deltaTime;
             rb.AddForce(new Vector2(0, Speed));
+            RotacaoEngrenagem();
         }
         if(time >= Maxtime)
         {
+            Instantiate(limitador2, posicional.transform.position, Quaternion.identity);
             time = 0;
             Aviso = false;
             Speed = 0;
+            velocidadeRotacionatoria = 0;
         }
+       
     }
     void Subir()
     {
         // rb.AddForce(new Vector2(0, Speed));
         Aviso = true;
+    }
+    void RotacaoEngrenagem()
+    {
+     
+        z = z + Time.deltaTime * velocidadeRotacionatoria;
+        engrenagem.transform.rotation = Quaternion.Euler(0, 0, z);
     }
 }
